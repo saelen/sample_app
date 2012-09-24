8fe49897ca495d32a0ed5663e7447c5071db1e3c
saelen <mterhaar@gmail.com>
2012-09-24 19:28:14 -0400

    sign ins

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,15 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: {case_sensitive: false}
-  before_save { |user| user.email = email.upcase }
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
+
+  before_save { |user| user.email = email.upcase }
+  before_save :create_remember_token
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
 end
